cd9959557a3679e0d9a3cc6f483bda9c6d9b6f55
James Farrow <james@james--sarahs-imac.local>
2017-04-24 10:17:13 +0100

    Initial Commit

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullCowGame
+//
+//  Created by James Farrow on 24/04/2017.
+//  Copyright © 2017 James Farrow. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
